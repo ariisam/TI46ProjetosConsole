142091ed63858efcafc651744a2fbf13ce8994e1
ariisam <ariisam97@gmail.com>
2024-09-30 21:22:53 -0300

    Versão 2 do aplicativo, mudanças no código...

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -10,6 +10,8 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Word !!");
+            Console.WriteLine("Teste, linha 2");
         }
     }
 }
